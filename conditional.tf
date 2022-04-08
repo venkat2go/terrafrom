b29f0757884a7ba85848065586ff7943bc1264a1
unknown <venkatesh.madesh@gmail.com>
2022-04-08 15:26:42 +0530

    test3

--- conditional.tf
+++ conditional.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "dev" {
    ami = "ami-064ff912f78e3e561"
-   instance_type = "t2.micro"
+   instance_type = "t3.small"
    tags = {
-   Name = "terraform-cloud-venkat"
+   Name = "terraform-cloud-venkat-2"
  }
 }
 
